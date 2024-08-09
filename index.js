2779d12a22893b07af537bec577d33452e88a2b5
Odser <92499450+Helticc@users.noreply.github.com>
2024-08-09 18:03:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const comRock = document.createElement('img');
 const comPaper = document.createElement('img');
 const comScissor = document.createElement('img');
 
+//-----buttons-rock-paper-scissors-----//
+
 const btnRock = document.createElement('button');
 btnRock.className = "rockBtn";
 const btnPaper = document.createElement('button');
@@ -14,28 +16,33 @@ btnPaper.className = "paperBtn";
 const btnScissor = document.createElement('button');
 btnScissor.className = "scissorBtn";
 
-const player = document.createElement('div');
+//-----score-information-----//
+
+const scorePlayer = document.createElement('div');
+scorePlayer.className = "Score-Player";
+
+const scoreComputer = document.createElement('div');
+scoreComputer.className = "Score-Computer";
+
+const scorePlayerText = document.createElement('div');
+scorePlayerText.className = "Score-Info";
+
+scorePlayerText.innerHTML = "Player score: ";
+
+const scoreComputerText = document.createElement('div');
+scoreComputerText.className = "Score-Info-Computer";
+
+scoreComputerText.innerHTML = "Computer score: ";
+
+//-----container-box-----//
+
+const player = document.createElement('img');
 player.className = "container";
 
-body.appendChild(player);
-body.appendChild(btnRock);
-body.appendChild(btnPaper);
-body.appendChild(btnScissor);
+const computerPlay = document.createElement('img');
+computerPlay.className = "container-pc";
 
-function buttonRock() {
-    player.appendChild(rock);
-}
-btnRock.onclick = buttonRock;
-
-function buttonPaper() {
-    player.appendChild(paper);
-}
-btnPaper.onclick = buttonPaper;
-
-function buttonScissor() {
-    player.appendChild(scissor);
-}
-btnScissor.onclick = buttonScissor;
+//-------elements-style-------//
 
 rock.src = "https://png.pngtree.com/png-clipart/20220824/ourmid/pngtree-cartoon-rock-transparent-png-image_6122807.png";
 rock.style.width = "100%";
@@ -45,5 +52,125 @@ scissor.src = "scissor.png";
 scissor.style.width = "100%";
 
 comRock.src = "https://png.pngtree.com/png-clipart/20220824/ourmid/pngtree-cartoon-rock-transparent-png-image_6122807.png";
+comRock.style.width = "100%";
 comPaper.src = "paper.png";
+comPaper.style.width = "100%";
 comScissor.src = "scissor.png";
+comScissor.style.width = "100%";
+
+//------body-tags------//
+
+body.appendChild(scorePlayer);
+body.appendChild(scoreComputer);
+body.appendChild(scorePlayerText);
+body.appendChild(scoreComputerText);
+body.appendChild(computerPlay);
+body.appendChild(player);
+body.appendChild(btnRock);
+body.appendChild(btnPaper);
+body.appendChild(btnScissor);
+
+function buttonRock() {
+    player.src = rock.src;
+
+    const randomNum = Math.ceil(Math.random() * 3);
+
+    if(randomNum == 1) {
+        console.log('tie')
+        computerPlay.src = comRock.src
+    }else if(randomNum == 2) {
+        console.log('lose')
+        computerPlay.src = comPaper.src
+    }else {
+        console.log('win')
+        computerPlay.src = comScissor.src
+    }
+
+
+}
+
+btnRock.onclick = buttonRock;
+
+function buttonPaper() {
+    player.src = paper.src;
+
+    const randomNum = Math.ceil(Math.random() * 3);
+
+    if(randomNum == 1) {
+        console.log('win')
+        computerPlay.src = comRock.src
+    }else if(randomNum == 2) {
+        console.log('tie')
+        computerPlay.src = comPaper.src
+    }else {
+        console.log('lose')
+        computerPlay.src = comScissor.src
+    }
+}
+
+btnPaper.onclick = buttonPaper;
+
+function buttonScissor() {
+    player.src = scissor.src;
+
+    const randomNum = Math.ceil(Math.random() * 3);
+
+    if(randomNum == 1) {
+        // console.log('lose')
+
+        computerPlay.src = comRock.src
+    }else if(randomNum == 2) {
+        console.log('win')
+        computerPlay.src = comPaper.src
+    }else {
+        console.log('tie')
+        computerPlay.src = comScissor.src
+    }
+}
+
+btnScissor.onclick = buttonScissor;
+
+//--------------------//
+
+// const body = document.getElementsByTagName('body')[0];
+// const paper = document.createElement('img');
+// const rock = document.createElement('img');
+// const scissor = document.createElement('img');
+
+
+// const box = document.createElement('div');
+// box.style.height = "300px";
+// box.style.width = "300px";
+// box.style.overflow = "hidden";
+// body.appendChild(box);
+
+// const button = document.createElement('button');
+// button.innerHTML = "START";
+// button.style.position = "absolute";
+// button.style.top = "350px";
+// button.style.left = "350px";
+// body.appendChild(button);
+
+// paper.src = 'paper.png';
+// paper.style.height = "300px";
+// paper.style.width = "300px";
+// rock.src = 'rock.png';
+// rock.style.height = "300px";
+// rock.style.width = "300px";
+// scissor.src = 'scissor.png';
+// scissor.style.height = "300px";
+// scissor.style.width = "300px";
+
+// function RPS() {
+//     const randomNum = Math.ceil(Math.random() * 3);
+
+//     if(randomNum === 1) {
+//         box.appendChild(paper);
+//     }else if(randomNum === 2) {
+//         box.appendChild(rock);
+//     }else if(randomNum === 3) {
+//         box.appendChild(scissor);
+//     }
+// }
+
+// button.onclick = RPS;
